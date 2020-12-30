aaf144cf009c1dd7f1a694c6636a38e952d31042
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-30 19:03:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ContaDeUmAteDez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Faça um programa que leia um número (inteiro e maior que zero) e escreva a sequência de zero ao número digitado.
+            
+            int i, n;
+            Console.WriteLine("Informe um numero inteiro maior que zero:");
+            i = int.Parse(Console.ReadLine());
+
+            for (n = 0; n <= i; n++)
+            {
+                Console.WriteLine("{0} ", n);
+            }
+        }
+    }
+}
